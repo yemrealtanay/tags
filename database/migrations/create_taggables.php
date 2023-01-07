a8058fb56230750d1fdd7c3c74473807603bee5f
yemrealtanay <y.emrealtanay@gmail.com>
2023-01-08 01:35:31 +0300

    migrations fixed

--- database/migrations/create_taggables.php
+++ database/migrations/create_taggables.php
@@ -4,21 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagTables extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id('tag_id)->autoIncrement();
-            $table->json('name');
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
         Schema::create('taggables', function (Blueprint $table) {
             $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
             $table->morphs('taggable');
             $table->unique(['tag_id', 'taggable_id', 'taggable_type']);
         });
     }
-}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+};
